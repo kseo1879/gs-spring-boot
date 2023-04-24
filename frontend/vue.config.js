f034a6a95bcd4d017cf49738d3b6acc984bd6b82
Calvin <kangmin.seo@honestfund.kr>
2023-04-24 23:13:24 +0900

    hm

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,10 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    proxy: 'http://44.215.203.222:3000/',
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST",
-    }
+    proxy: 'http://44.215.203.222:3000/'
   }
 })
