6ed98c95887b7824c6030b587dc70badf35da4af
Calvin <kangmin.seo@honestfund.kr>
2023-04-24 22:55:29 +0900

    hmm

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
   configureWebpack: {
     devServer: {
       headers: { "Access-Control-Allow-Origin": "*" }
