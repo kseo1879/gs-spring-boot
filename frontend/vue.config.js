6d37bd5756cbc12d25d72335d854391df0f2fc1c
Calvin <kangmin.seo@honestfund.kr>
2023-04-24 23:06:27 +0900

    plseuen;afklj

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,6 +3,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
     devServer: {
+      proxy: 'http://localhost:8080/',
       headers: {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET, POST",
