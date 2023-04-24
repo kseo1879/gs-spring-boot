e43dbe1d578ea4cbdd53d9a6b7b8c07e10315229
Calvin <kangmin.seo@honestfund.kr>
2023-04-24 21:14:06 +0900

    cors

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  webServer: {
+  devServer: {
     proxy: 'http://localhost:3000'
   }
 })
