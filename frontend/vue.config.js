7fdca358ec42644e7f08bc052d4aeb0170b55063
Calvin <kangmin.seo@honestfund.kr>
2023-04-24 21:21:28 +0900

    typo

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    proxy: 'http://localhose:8080/'
+    proxy: 'http://localhost:8080/'
   }
 })
