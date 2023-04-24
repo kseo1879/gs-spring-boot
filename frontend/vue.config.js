813672d3884b77fc4992f11dc569144989e4182e
Calvin <kangmin.seo@honestfund.kr>
2023-04-24 21:34:47 +0900

    hah...

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    proxy: 'http://44.215.203.222:3000/'
+    proxy: 'http://44.215.203.222:3000'
   }
 })
